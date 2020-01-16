ab36fb3c12335b71d020697632e76528d642ea03
VinhNQ <nqvinh@ecpay.vn>
2020-01-16 15:23:27 +0700

    reiview code

--- app/src/main/java/vn/ecpay/ewallet/common/base/BaseEventBus.java
+++ app/src/main/java/vn/ecpay/ewallet/common/base/BaseEventBus.java
@@ -1,23 +1,23 @@
 package vn.ecpay.ewallet.common.base;
 
 public class BaseEventBus {
-    public String mRouter;
-    public String mEventName;
+  public String mRouter;
+  public String mEventName;
 
-    public BaseEventBus(String router, String eventName) {
-        mRouter = router;
-        mEventName = eventName;
-    }
+  public BaseEventBus(String router, String eventName) {
+    mRouter = router;
+    mEventName = eventName;
+  }
 
-    public String getRouter() {
-        return mRouter;
-    }
+  public String getRouter() {
+    return mRouter;
+  }
 
-    public void setRouter(String mRouter) {
-        this.mRouter = mRouter;
-    }
+  public void setRouter(String mRouter) {
+    this.mRouter = mRouter;
+  }
 
-    public String getEventName() {
-        return mEventName;
-    }
+  public String getEventName() {
+    return mEventName;
+  }
 }
--- app/src/main/java/vn/ecpay/ewallet/common/eccrypto/ECElGamal.java
+++ app/src/main/java/vn/ecpay/ewallet/common/eccrypto/ECElGamal.java
@@ -1,36 +1,1 @@
-package vn.ecpay.ewallet.common.eccrypto;
-
-
-import org.bouncycastle.crypto.params.ECPrivateKeyParameters;
-import org.bouncycastle.crypto.params.ECPublicKeyParameters;
-import org.bouncycastle.math.ec.ECPoint;
-
-import java.math.BigInteger;
-
-/**
- * Created by Joe on July, 13 2018 .
- */
-public class ECElGamal {
-    public static byte[][] encrypt(EllipticCurve ec, ECPublicKeyParameters kP, byte[] rawData) {
-        BigInteger n = ec.randomD();
-        ECPoint ecPoint = ec.randomQ();
-        ECPoint m1 = ec.getGPoint().multiply(n);
-        ECPoint m2 = ecPoint.add(kP.getQ().multiply(n));
-        byte[] keyAES = new byte[32];
-        byte[] qArray = ecPoint.getEncoded(false);
-        System.arraycopy(qArray, qArray.length - 32, keyAES, 0, 32);
-
-        return new byte[][]{m1.getEncoded(false), m2.getEncoded(false), AES.encrypt(rawData, keyAES)};
-    }
-
-    public static byte[] decrypt(EllipticCurve ec, ECPrivateKeyParameters kS, byte[][] blockEncrypted) {
-        ECPoint m1 = ec.decodePoint(blockEncrypted[0]);
-        ECPoint m2 = ec.decodePoint(blockEncrypted[1]);
-        ECPoint ecPoint = m2.subtract(m1.multiply(kS.getD()));
-        byte[] keyAES = new byte[32];
-        byte[] qArray = ecPoint.getEncoded(false);
-        System.arraycopy(qArray, qArray.length - 32, keyAES, 0, 32);
-
-        return AES.decrypt(blockEncrypted[2], keyAES);
-    }
-}
+package vn.ecpay.ewallet.common.eccrypto;import org.bouncycastle.crypto.params.ECPrivateKeyParameters;import org.bouncycastle.crypto.params.ECPublicKeyParameters;import org.bouncycastle.math.ec.ECPoint;import java.math.BigInteger;/** * Created by Joe on July, 13 2018 . */public class ECElGamal {    public static byte[][] encrypt(EllipticCurve ec, ECPublicKeyParameters kP, byte[] rawData) {        BigInteger n = ec.randomD();        ECPoint ecPoint = ec.randomQ();        ECPoint m1 = ec.getGPoint().multiply(n);        ECPoint m2 = ecPoint.add(kP.getQ().multiply(n));        byte[] keyAES = new byte[32];        byte[] qArray = ecPoint.getEncoded(false);        System.arraycopy(qArray, qArray.length - 32, keyAES, 0, 32);        return new byte[][]{m1.getEncoded(false), m2.getEncoded(false), AES.encrypt(rawData, keyAES)};    }    public static byte[] decrypt(EllipticCurve ec, ECPrivateKeyParameters kS, byte[][] blockEncrypted) {        ECPoint m1 = ec.decodePoint(blockEncrypted[0]);        ECPoint m2 = ec.decodePoint(blockEncrypted[1]);        ECPoint ecPoint = m2.subtract(m1.multiply(kS.getD()));        byte[] keyAES = new byte[32];        byte[] qArray = ecPoint.getEncoded(false);        System.arraycopy(qArray, qArray.length - 32, keyAES, 0, 32);        return AES.decrypt(blockEncrypted[2], keyAES);    }}
