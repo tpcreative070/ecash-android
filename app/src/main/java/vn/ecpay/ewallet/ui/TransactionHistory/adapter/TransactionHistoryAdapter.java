f20e07aeac7512288c2b304bbceea1146b24ee90
vohoangtuit <vohoangtu2401@gmail.com>
2020-03-02 08:43:55 +0700

    fix bug: history

--- app/src/main/java/vn/ecpay/ewallet/ui/TransactionHistory/adapter/TransactionHistoryAdapter.java
+++ app/src/main/java/vn/ecpay/ewallet/ui/TransactionHistory/adapter/TransactionHistoryAdapter.java
@@ -93,7 +93,7 @@ public class TransactionHistoryAdapter extends RecyclerView.Adapter<RecyclerView
             itemViewHolder.tvTransactionStatus.setText(context.getString(R.string.str_fail));
         }
         itemViewHolder.tvTransactionDate.setText(CommonUtils.getDateTransfer(context, transactionsHistoryModel.getTransactionDate()));
-
+        Log.e("item ",new Gson().toJson(transactionsHistoryModel));
         switch (transactionsHistoryModel.getTransactionType()) {
             case TYPE_SEND_ECASH_TO_EDONG:
                 itemViewHolder.tvTransactionType.setText(context.getString(R.string.str_cash_out));
@@ -141,15 +141,15 @@ public class TransactionHistoryAdapter extends RecyclerView.Adapter<RecyclerView
             case TYPE_PAYTO:
                 //Log.e("payto ",new Gson().toJson(transactionsHistoryModel));
                 if (transactionsHistoryModel.getCashLogType().equals(Constant.STR_CASH_IN)) {
-//                    itemViewHolder.tvFullName.setText(context.getString(R.string.str_item_transfer_cash,
-//                            transactionsHistoryModel.getSenderName(), String.valueOf(transactionsHistoryModel.getSenderAccountId())));
+                    itemViewHolder.tvFullName.setText(context.getString(R.string.str_item_transfer_cash,
+                            transactionsHistoryModel.getSenderName(), String.valueOf(transactionsHistoryModel.getSenderAccountId())));
                     itemViewHolder.tvTransactionType.setText(context.getString(R.string.str_payment));
                     itemViewHolder.ivTransactionIcon.setImageResource(R.drawable.ic_payto_gray);
                     itemViewHolder.tvTransactionAmount.setText(context.getString(R.string.str_type_cash_in,
                             CommonUtils.formatPriceVND(Long.valueOf(transactionsHistoryModel.getTransactionAmount()))));
                 } else {
-//                    itemViewHolder.tvFullName.setText(context.getString(R.string.str_item_transfer_cash,
-//                            transactionsHistoryModel.getReceiverName(), String.valueOf(transactionsHistoryModel.getReceiverAccountId())));
+                    itemViewHolder.tvFullName.setText(context.getString(R.string.str_item_transfer_cash,
+                            transactionsHistoryModel.getReceiverName(), String.valueOf(transactionsHistoryModel.getReceiverAccountId())));
                     itemViewHolder.tvTransactionType.setText(context.getString(R.string.str_payment));
                     itemViewHolder.ivTransactionIcon.setImageResource(R.drawable.ic_payto_gray);
                     itemViewHolder.tvTransactionAmount.setText(context.getString(R.string.str_type_cash_out,
